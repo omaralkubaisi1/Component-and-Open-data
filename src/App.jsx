287af7713247466c92a62eb1eb2eeb8c707fd009
alkubaisiothman <othman_hotman@hotmail.com>
2024-10-22 19:17:16 +0300

    Css ja App.jsx -Css muutokset OpenDataa tehtävää varten -App.jsx muutokset

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ComponentUI from './ComponentUI';
 import OrderForm from './OrderForm';
+import OpenData from './OpenData';  
 
 function App() {
     return (
         <>
-            <ComponentUI image="/Car.jpg" title="Welcome to  product page!" />
+            <ComponentUI image="/Car.jpg" title="Welcome to product page!" />
             <OrderForm />
+            <OpenData /> {}
         </>
     );
 }
