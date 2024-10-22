1ee059dcf700d79a4c03aece3f72f4176d0f554e
alkubaisiothman <othman_hotman@hotmail.com>
2024-10-22 19:19:52 +0300

    AppRouter -AppRouter koodi luotu

---
+++ src/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import OpenData from "./OpenData";
+import Home from "./Home";
+import ComponentUI from './ComponentUI';
+import OrderForm from './OrderForm';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product" element={
+                    <>
+                        <ComponentUI image="/sunset.jpg" title="Welcome to product page!" />
+                        <OrderForm />
+                    </>
+                } />
+                <Route path="/opendata" element={<OpenData />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
